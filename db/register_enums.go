b8760b61bb50ae16b204ecd267c81eca9075f5c5
Valeryia Andraichuk <andraichuk.valeryia@gmail.com>
2024-01-27 17:47:16 +0100

    Register enums on db start

--- db/register_enums.go
+++ db/register_enums.go
@@ -6,17 +6,32 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func RegisterEnums(ctx context.Context, conn *pgx.Conn) error {
-	t, err := conn.LoadType(ctx, "course")
-	if err != nil {
-		return err
-	}
-	conn.TypeMap().RegisterType(t)
+var atomic_enums = []string{"application_status", "gender", "language_proficiency", "study_degree", "study_program"}
+var compose_enums = []string{"course", "device"}
 
-	t, err = conn.LoadType(ctx, "_course")
-	if err != nil {
-		return err
+func RegisterEnums(ctx context.Context, conn *pgx.Conn) error {
+	// Register atomic enums
+	for _, enum := range atomic_enums {
+		t, err := conn.LoadType(ctx, enum)
+		if err != nil {
+			return err
+		}
+		conn.TypeMap().RegisterType(t)
 	}
-	conn.TypeMap().RegisterType(t)
+
+	// Register compose enums
+	for _, enum := range compose_enums {
+		t, err := conn.LoadType(ctx, enum)
+		if err != nil {
+			return err
+		}
+		conn.TypeMap().RegisterType(t)
+
+		t, err = conn.LoadType(ctx, "_"+enum)
+		if err != nil {
+			return err
+		}
+		conn.TypeMap().RegisterType(t)
+	}
 	return nil
 }
